577f6d67e2479143b09f850057724e962f949fee
Emil Okkels Klein <eok@pentia.dk>
2016-12-19 15:05:54 +0100

    added auto-publish-views and initiated project. version 0.1.0

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require("gulp");
+var publish = require("@pentia/publish-projects")
+var foreach = require("gulp-foreach");
+
+gulp.task("Auto-Publish-Views", function () {
+  var root = "./src";
+  var roots = [root + "/**/Views", "!" + root + "/**/obj/**/Views"];
+  var files = "/**/*.cshtml";
+
+  gulp.src(roots, { base: root }).pipe(
+    foreach(function (stream, rootFolder) {
+      gulp.watch(rootFolder.path + files, function (event) {
+        if (event.type === "changed") {
+          console.log("file: " + event.path + " has changed");
+          var viewPathIndex = event.path.toLocaleLowerCase().indexOf("views");
+          var projectPath = event.path.slice(0,viewPathIndex);
+          publish.publish.publishProjects(projectPath);
+        }
+      });
+      return stream;
+    })
+  );
+});

22176d0aa4091623a660800210cb25d8a82bdfc0
Emil Okkels Klein <eok@pentia.dk>
2017-01-17 10:25:32 +0100

    simplify watch, add support for running multiple times

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,22 @@
 var gulp = require("gulp");
 var publish = require("@pentia/publish-projects")
 var foreach = require("gulp-foreach");
+var path = require("path");
+var glob = require("glob");
 
 gulp.task("Auto-Publish-Views", function (callback) {
   var root = "./src";
-  var roots = [root + "/**/Views", "!" + root + "/**/obj/**/Views"];
-  var files = "/**/*.cshtml";
+  var roots = ["./src/**/Views/**/*.cshtml", "!./src/**/obj/**/*.cshtml"];
 
-  gulp.src(roots, { base: root }).pipe(
-    foreach(function (stream, rootFolder) {
-      gulp.watch(rootFolder.path + files, function (event) {
-        if (event.type === "changed") {
-          console.log("file: " + event.path + " has changed");
-          var viewPathIndex = event.path.toLocaleLowerCase().indexOf("views");
-          var projectPath = event.path.slice(0,viewPathIndex);
-          publish.publish.publishProjects(projectPath);
-          publish.deleteConfig.delete(callback);
-        }
-      });
-      return stream;
-    })
-  );
+	gulp.watch(roots, function (event) {
+		console.log(event);
+		if (event.type === "changed") {
+			console.log("file: " + event.path + " has changed");
+			var viewPathIndex = event.path.toLocaleLowerCase().indexOf("views");
+			var projectPath = event.path.slice(0,viewPathIndex);
+			publish.publish.publishProjects(projectPath);
+			publish.deleteConfig.delete();
+		}
+	});
 });
+

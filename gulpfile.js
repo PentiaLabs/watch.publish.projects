6155073d97650ca10611754911f6d20aac6fd3d8
Allan Kimmer Jensen <allankimmerjensen@gmail.com>
2017-02-06 11:42:57 +0100

    Clean up task and dependencies
    This cleans up all unused vars, and adds better logging via gulp utils.

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,17 @@
-var gulp = require("gulp");
-var publish = require("@pentia/publish-projects")
-var foreach = require("gulp-foreach");
-var path = require("path");
-var glob = require("glob");
+var gulp = require('gulp');
+var publish = require('@pentia/publish-projects');
+var gutil = require('gulp-util');
 
-gulp.task("Auto-Publish-Views", function (callback) {
-  var root = "./src";
-  var roots = ["./src/**/Views/**/*.cshtml", "!./src/**/obj/**/*.cshtml"];
+gulp.task('Auto-Publish-Views', function () {
+	var roots = ['./src/**/Views/**/*.cshtml', '!./src/**/obj/**/*.cshtml'];
 
 	gulp.watch(roots, function (event) {
-		console.log(event);
-		if (event.type === "changed") {
-			console.log("file: " + event.path + " has changed");
-			var viewPathIndex = event.path.toLocaleLowerCase().indexOf("views");
+		gutil.log('File ' +  gutil.colors.blue(event.path) + ' has ' + event.type);
+		if (event.type === 'changed') {
+			var viewPathIndex = event.path.toLocaleLowerCase().indexOf('views');
 			var projectPath = event.path.slice(0,viewPathIndex);
 			publish.publish.publishProjects(projectPath);
 			publish.deleteConfig.delete();
 		}
 	});
 });
-
